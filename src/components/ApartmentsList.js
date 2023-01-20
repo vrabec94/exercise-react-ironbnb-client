262cebcc20d6ec09e981cb1e7b47e754b101a07a
Laura Bährs <laura.baehrs@googlemail.com>
2023-01-20 14:54:36 +0100

    implement apartmentList, iteration 2

--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -1,6 +1,43 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
 function ApartmentsList(){
+
+    const [apartmentArray, setApartmentArray] = useState(null);
+
+    useEffect(() => {
+      getApartmentsFromApi();
+    }, []);
+    
+    const url = "https://ironbnb-m3.herokuapp.com/apartments"
+  
+    const getApartmentsFromApi = () => {
+      axios.get(url)
+        .then((response) => {
+          console.log(response.data);
+  
+          setApartmentArray(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+
     return(
+        <>
         <h1>This is ApartmentsList</h1>
+        {apartmentArray === null
+                ? "loading..."
+                : apartmentArray.map((apartmentDetails, index) => {
+                    return (
+                        <div className="character" key={apartmentDetails._id} >
+                            <h2>{apartmentDetails.title}</h2>
+                            {/*<Link to={"/characters/" + apartmentDetails.id}>More Details</Link>*/}
+                        </div>
+                    )
+                })}
+        </>
     )
 }
 
